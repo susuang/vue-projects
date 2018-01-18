e94d5b5a063c6c302bdd462f42bb2bb9a3e544d6
yellow yellow cat <30364534+susuang@users.noreply.github.com>
2018-01-18 16:43:22 +0800

    component header
    but now it dont be used

---
+++ ShiVue/src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="header-item">
+        <div class="leftBtn"></div>
+        <div class="title">{{title}}</div>
+        <div class="rightBtn"></div>
+    </header>
+</template>
+
+<script>
+    export default {
+        props: ['title'],
+    }
+</script>
