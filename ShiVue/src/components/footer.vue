dd0c48a96286bafbfedf2e8108c72470929cc82a
yellow yellow cat <30364534+susuang@users.noreply.github.com>
2018-01-18 16:42:07 +0800

    component footer
    it include tabs

---
+++ ShiVue/src/components/footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="app-footer">
+        <router-link to="/Page1">商品1</router-link>
+        <router-link to="/ratings">评论</router-link>
+        <router-link to="/seller">商家</router-link>
+    </footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss">
+    .app-footer {
+        @include display-flex();
+        @include justify-content(space-between);
+        @include align-items(center);
+        width: 100%;
+        height: 2.2rem;
+        a {
+            display: inline-block;
+            flex: 1;
+            text-align: center;
+        }
+    }
+</style>
