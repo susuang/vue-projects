5eafe6807683d57feb9dcf0df373f8b9776b8835
yellow yellow cat <30364534+susuang@users.noreply.github.com>
2018-01-18 16:50:25 +0800

    common

---
+++ ShiVue/src/js/constant.js
@@ -0,0 +1,5 @@
+let constant = {
+    "isLogin": false,
+    "version": ""
+};
+export default constant;
---
+++ ShiVue/src/page/login.vue
@@ -0,0 +1,89 @@
+<template>
+    <app-layout showFooter="fasle">
+        <div slot="header" class="header">
+            <div class="title">汇报登录</div>
+        </div>
+        <div class="login-form">
+            <div class="login-inputs">
+                <span><input type="text" placeholder="User:" name="username" v-model="username"></span>
+                <span><input type="password" placeholder="Password:" name="password" v-model="password"></span>
+                <button type="button" class="button btnBlock-blue" @click="login">登录</button>
+                <button @click="openLoading()">loading</button>
+                <div id="info">{{loginTip}}</div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data(){
+        return {
+            username: '',
+            password: '',
+            loginTip: ''
+        }
+    },
+    methods: {
+        async login() {
+            if(this.username === '' || this.password === ''){
+                this.loginTip = '请正确填写用户名和密码';
+            }
+        },
+        openLoading(){
+        this.$loading.open('loading...');
+        let self = this;
+        setTimeout(function () {
+          self.closeLoading()
+        }, 2000)
+      },
+      closeLoading(){
+        this.$loading.close();
+      }
+    }
+} 
+</script>
+
+<style lang="scss">
+    .login-form {
+        width: 100%;
+        height: 100%;
+        background: _palette(login-bg);
+        text-align: center;
+        span {
+            display: block;
+            padding: 8px;
+            border-bottom: solid 2px #34455d; 
+        }
+        input {
+            width: 90%;
+            height: 1.6rem;
+            font-size: 16px;
+            color: #fff;
+            background: transparent;
+            border: none;
+            &::-webkit-input-placeholder {
+                color: #fff;
+            }
+            &:-moz-placeholder {
+                color: #fff;
+            }
+            &::-moz-placeholder {
+                color: #fff;
+            }
+            &:-ms-input-placeholder {
+                color: #fff;
+            }
+        }
+        button {
+            margin-top: 1.5rem;
+            width: 100%;
+        }
+    }
+    .login-inputs {
+        margin: 0 auto;
+        padding-top: 2rem;
+        width: 90%;
+    }
+</style>
