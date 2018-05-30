3d2c1006eb46dcfe3b4473f77564110a257e1bbd
yellow yellow cat <30364534+susuang@users.noreply.github.com>
2018-05-30 16:58:28 +0800

    chartproject

---
+++ chartproject/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
